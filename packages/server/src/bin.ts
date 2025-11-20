7e10843beff48756dd75e21eca0521cecbf3c0da
David Buchanan <david@blueskyweb.xyz>
2025-11-20 12:59:06 +0000

    start a sequence leader in bin.ts

--- packages/server/src/bin.ts
+++ packages/server/src/bin.ts
@@ -1,15 +1,24 @@
 import './env'
 import { Database, PlcDatabase } from './db'
 import PlcServer from '.'
+import { SequencerLeader } from './sequencer'
 
 const run = async () => {
   const dbUrl = process.env.DATABASE_URL
 
   let db: PlcDatabase
+  let leader: SequencerLeader | undefined
   if (dbUrl) {
     const pgDb = Database.postgres({ url: dbUrl })
     await pgDb.migrateToLatestOrThrow()
     db = pgDb
+
+    // Start sequencer leader
+    leader = new SequencerLeader(pgDb)
+    leader.run().catch((err) => {
+      console.error('Sequencer leader error:', err)
+    })
+    console.log('[*] Sequencer leader started')
   } else {
     db = Database.mock()
   }
@@ -21,6 +30,17 @@ const run = async () => {
   const plc = PlcServer.create({ db, port, adminSecret })
   await plc.start()
   console.log(`👤 PLC server is running at http://localhost:${port}`)
+
+  // Graceful shutdown
+  const shutdown = async () => {
+    console.log('\nShutting down...')
+    leader?.destroy()
+    await plc.destroy()
+    process.exit(0)
+  }
+
+  process.on('SIGINT', shutdown)
+  process.on('SIGTERM', shutdown)
 }
 
 run()
